2ee7033abf92d8c208d77f97297ab401acad792f
Sadiya <sadiyaayub16@gmail.com>
2023-09-30 10:43:23 +0530

    update

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ viewImagesLinks.forEach(link => {
         }
     });
 });
+
+
+
+// Get the elements by their IDs
+var phoneNumber = document.getElementById("phone-number");
+var showPhoneLink = document.getElementById("show-phone");
+
+// Add a click event listener to the "Show" link
+showPhoneLink.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    // Toggle the visibility of the phone number by adding/removing the "hidden" class
+    if (phoneNumber.classList.contains("hidden")) {
+        phoneNumber.classList.remove("hidden"); // Display the phone number
+        showPhoneLink.textContent = "Hide"; // Update the link text
+    } else {
+        phoneNumber.classList.add("hidden"); // Hide the phone number
+        showPhoneLink.textContent = "Show"; // Update the link text
+    }
+});
